eddba71545a873e9bc293c62f117513aa3223f93
Zach Garwood <iam@zachgarwood.com>
2023-08-25 13:04:52 -0500

    Add message when no audio file exists [MA-123]

--- resources/views/admin/audio-controls.blade.php
+++ resources/views/admin/audio-controls.blade.php
@@ -1,1 +1,5 @@
-<audio controls src="{{ $src }}" @style(['width: 100%'])></audio>
+@if ($src)
+    <audio controls src="{{ $src }}" @style(['width: 100%'])></audio>
+@else
+    <span>No audio available</span>
+@endif
