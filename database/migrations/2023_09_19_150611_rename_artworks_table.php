4a6e5aa15223e71fc9f5c60484279941d3709c7d
Zach Garwood <iam@zachgarwood.com>
2023-09-19 10:26:21 -0500

    Rename Artwork model to CollectionObject [MA-125]

---
+++ database/migrations/2023_09_19_150611_rename_artworks_table.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::rename('artworks', 'collection_objects');
+    }
+
+    public function down(): void
+    {
+        Schema::rename('collection_objects', 'artworks');
+    }
+};
