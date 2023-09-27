52cb028e82766f06d63621220cd4280a73339b6b
Zach Garwood <iam@zachgarwood.com>
2023-09-27 15:32:40 -0500

    Replace example test with authentication test

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,45 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use A17\Twill\Models\User as TwillUser;
 
-class User extends Authenticatable
+class User extends TwillUser
 {
-    use HasApiTokens;
     use HasFactory;
-    use Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -2,15 +2,19 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Support\Collection;
 use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Laravel\Dusk\Browser;
 use Laravel\Dusk\TestCase as BaseTestCase;
 
 abstract class DuskTestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected ?User $user = null;
 
     /**
      * Prepare for Dusk test execution.
@@ -27,6 +31,25 @@ abstract class DuskTestCase extends BaseTestCase
         }
     }
 
+    public function user()
+    {
+        return $this->user ?? User::factory()->create();
+    }
+
+    /**
+     * TODO: Figure out how to get $this->login() and logout() to work with the
+     * Twill User model. This simulates the functionality of login() by
+     * manually visiting the long page and filling in the form.
+     */
+    protected function authenticate(): void
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin')
+                ->type('email', $this->user()->email)
+                ->type('password', 'password')
+                ->press('Login');
+        });
+    }
     /**
      * Create the RemoteWebDriver instance.
      */
