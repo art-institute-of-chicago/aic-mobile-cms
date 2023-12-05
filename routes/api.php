b139611b5f404960e2c5bab544d36465078addea
Zach Garwood <iam@zachgarwood.com>
2023-12-05 16:36:35 -0600

    Correct "filename" for json data route [MA-88]

--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/appData.json', function () {
+Route::get('/appData-v3.json', function () {
     // Listed in the order the sections appear in the Drupal appData.json
     $controllers = collect([
         Dashboard::class,
