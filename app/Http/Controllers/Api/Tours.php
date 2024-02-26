d16c2a0325019569d9bb092d6683a598ccd7a738
nikhiltri <ntrivedi@artic.edu>
2024-02-26 12:25:31 -0600

    Add caching to `/tours` API calls [MA-159]

--- app/Http/Controllers/Api/Tours.php
+++ app/Http/Controllers/Api/Tours.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\Controller;
 use App\Repositories\Serializers\TourSerializer;
 use App\Repositories\TourRepository;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 
 class Tours extends Controller
 {
     public function __invoke()
     {
         $repository = App::make(TourRepository::class);
-        $tours = $repository->getBaseModel()->newQuery()->visible()->published()->get();
+        $tours = Cache::remember('tourRepo-all', 300, function () use ($repository) { return $repository->getBaseModel()->newQuery()->visible()->published()->get(); });
         $serializer = new TourSerializer();
         return $serializer->serialize($tours);
     }
